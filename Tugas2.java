57fd9359a68e56911cc6476643e2ee9528e7b7e1
Okitora Winnetou <98197185+wikitora99@users.noreply.github.com>
2022-08-01 18:29:45 +0700

    Update Tugas2.java

--- Tugas2.java
+++ Tugas2.java
@@ -4,36 +4,30 @@ class Tugas2
 {
     static Scanner scr = new Scanner(System.in);
 
-    private static boolean isPrimeNumber(int n)
+    private static String isPrimeNumber(int n)
     {
         if (n < 2){
-            return false;
+            return "bukan bilangan primer";
         }
 
         for (int i = 2; i <= Math.sqrt(n); i++){
             if (n % i == 0){
-                return false;
+                return "bukan bilangan primer;
             }
         }
 
-        return true;
+        return "adalah bilangan primer";
     }
 
     public static void main(String[] args) 
     {    
-        int n;
-
         System.out.print("\n======================\n");
-        System.out.print("Masukkan angka: "); n = scr.nextInt();
+        System.out.print("Masukkan angka: "); int n = scr.nextInt();
         System.out.print("========================\n\n");
 
         for (int i = 0; i <= n; i++)
         {
-            if (isPrimeNumber(i)){
-                System.out.println(i+" adalah bilangan primer");
-            }else{
-                System.out.println(i+" bukan bilangan primer");
-            }
+            System.out.println(i+" "+isPrimeNumber(i);
         }
     }
 }
